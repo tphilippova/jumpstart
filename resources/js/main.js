549244aef650fc958232b3484e0f3ce3ef3512eb
unknown <tphilippova@gmail.com>
2018-11-13 23:30:30 -0600

    Add slider. Fix bugs.

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+  $(document).ready(function() {
+    $('.carousel').slick({
+      slidesToShow: 1,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      draggable: false,
+      pauseOnFocus: false,
+      pauseOnHover: false
+    });
+
+    var prevScroll = 0
+    $('')
+  });
