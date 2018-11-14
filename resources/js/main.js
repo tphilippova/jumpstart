6aa6de4f6123bd22ced57d68ab5bfb59ac7ea32c
unknown <tphilippova@gmail.com>
2018-11-14 00:04:12 -0600

    Fix bugs: Text and button position in slider. Add 'hide' function for navigation.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,6 @@
-  $(document).ready(function() {
+  $(document).ready(function(){
     $('.carousel').slick({
-      slidesToShow: 1,
-      infinite: true,
+      slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 4000,
       arrows: false,
@@ -10,6 +9,16 @@
       pauseOnHover: false
     });
 
-    var prevScroll = 0
-    $('')
+    var lastScrollTop = 0;
+    $(window).scroll(function(){
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop - lastScrollTop > 50){
+        var navHeight = $('.navbar').css('height');
+        $('.navbar').animate({top: '-' + navHeight}, 150);
+        lastScrollTop = scrollTop;
+      } else if (lastScrollTop - scrollTop > 50) {
+        $('.navbar').animate({top: '0px'}, 150);
+        lastScrollTop = scrollTop;
+      }
+    });
   });
